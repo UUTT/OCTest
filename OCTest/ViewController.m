a1be2762e8bde6029e9fe4a904c1734da9f324fe
xxxx <xxx@163.com>
2020-04-27 14:24:36 +0800

    iOS中url的特殊字符的转换

--- OCTest/ViewController.m
+++ OCTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
 
@@ -16,8 +17,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+//  1、  iOS中url的特殊字符的转换
+    NSString *urlString=[NSString stringWithFormat:@"http://www.baidu.com/?param=%@",@"中文http://dd.com"];
+    
+    urlString = CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (CFStringRef)urlString, nil, CFSTR(":/?#[]@!$&’()*+,;="), kCFStringEncodingUTF8));
+    
+    NSMutableCharacterSet *charset = [[NSCharacterSet URLQueryAllowedCharacterSet] mutableCopy];
+    [charset removeCharactersInString:@"?:;/=&"];
+    urlString = [urlString stringByAddingPercentEncodingWithAllowedCharacters:charset];
+    
+    NSString *charactersToEscape = @"?!@#$^&%*+,:;='\"`<>()[]{}/\\| ";
+    NSCharacterSet *allowedCharacters = [[NSCharacterSet characterSetWithCharactersInString:charactersToEscape] invertedSet];
+    urlString = [urlString stringByAddingPercentEncodingWithAllowedCharacters:allowedCharacters];
+    NSLog(@"%@",urlString);
+    
+//  2、 获取系统音量并实时监测音量变化
+    NSError *error;
+    [[AVAudioSession sharedInstance] setActive:YES error:&error];
+    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+    
+    CGFloat volume =  [[AVAudioSession sharedInstance] outputVolume];
+    NSLog(@"系统音量%f",volume);
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(systemVolumeDidChangeNoti:) name:@"AVSystemController_SystemVolumeDidChangeNotification" object:nil];
 }
 
+-(void)systemVolumeDidChangeNoti:(NSNotification* )notifi{
+    NSString * style = [notifi.userInfo objectForKey:@"AVSystemController_AudioCategoryNotificationParameter"];
+       CGFloat value = [[notifi.userInfo objectForKey:@"AVSystemController_AudioVolumeNotificationParameter"] doubleValue];
+       if ([style isEqualToString:@"Ringtone"]) {
+           NSLog(@"铃声改变");
+       }else if ([style isEqualToString:@"Audio/Video"]){
+           NSLog(@"音量改变 当前值:%f",value);
+       }
+   
+}
 
 @end
